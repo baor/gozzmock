217b4311033a166d52b4b25fe6330346fe0c7369
Igor Rybak <irybak@travix.com>
2017-08-30 13:22:33 +0200

    update build script to include coverage

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "golint validations..."
+golint .
+golint ./controller/...
+golint ./handler/...
+golint ./model/...
 
 echo "go clean ..."
 go clean -i ./...
@@ -10,13 +15,11 @@ go get ./...
 echo "go build ..."
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o gozzmock_bin .
 
-echo "go test ..."
-go test ./...
+echo "go test and coverage..."
+go test -coverprofile=cover_controller.out ./controller
+go test -coverprofile=cover_handler.out ./handler
 
-#set -e
-
-#echo "Retrieving/updating vendor packages..."
-
-#gvt update google.golang.org/api/pubsub/v1 || true
+go tool cover -html=cover_controller.out -o cover_controller.html
+go tool cover -html=cover_handler.out -o cover_handler.html
 
 echo "Done!"
