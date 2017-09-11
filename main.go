ab9cba2134c9f752ab84ba74c5c5d0c9236f77a2
Igor Rybak <irybak@travix.com>
2017-09-11 22:19:52 +0200

    set log level as an argument

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
@@ -25,12 +26,40 @@ func httpHandleFuncWithLogs(pattern string, handler func(http.ResponseWriter, *h
 	http.HandleFunc(pattern, wrappedHandler)
 }
 
+func setZeroLogLevel(logLevel string) {
+
+	selectedLevel := zerolog.DebugLevel
+
+	switch logLevel {
+	case "debug":
+		selectedLevel = zerolog.DebugLevel
+	case "info":
+		selectedLevel = zerolog.InfoLevel
+	case "warn":
+		selectedLevel = zerolog.WarnLevel
+	case "error":
+		selectedLevel = zerolog.ErrorLevel
+	case "fatal":
+		selectedLevel = zerolog.FatalLevel
+	case "panic":
+		selectedLevel = zerolog.PanicLevel
+	}
+	fmt.Println("set log level:", selectedLevel)
+	zerolog.SetGlobalLevel(selectedLevel)
+}
+
 func main() {
 	var initExpectations string
 	flag.StringVar(&initExpectations, "expectations", "[]", "set initial expectations")
+	var logLevel string
+	flag.StringVar(&logLevel, "loglevel", "debug", "set log level: debug, info, warn, error, fatal, panic")
 	flag.Parse()
+
 	fmt.Println("initial expectations:", initExpectations)
+	fmt.Println("loglevel:", logLevel)
 	fmt.Println("tail:", flag.Args())
+
+	setZeroLogLevel(logLevel)
 
 	exps := ExpectationsFromString(initExpectations)
 
